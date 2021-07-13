53fd268210b542d97da7c4259b81c2dbb756e213
Sarveshwar Singh <shin@shinlabs.net>
2021-07-13 19:09:15 +0900

    add static site

---
+++ src/views/pool/sumo/Metadata.vue
@@ -0,0 +1,11 @@
+<script>
+// @ is an alias to /src
+import MetadataJSON from './metadata.json';
+
+document.body.appendChild(MetadataJSON);
+
+// export default {
+//   name: 'Home',
+//   components: MetadataJSON,
+// };
+</script>
